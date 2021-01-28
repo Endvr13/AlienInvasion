303ac24ac653c5932279d7601ed6582fbbd5b899
Endvr13 <endvrtrade@gmail.com>
2021-01-28 00:06:08 +0000

    First Commit

---
+++ settings.py
@@ -0,0 +1,18 @@
+class Settings:
+
+    def __init__(self):
+
+        # Window Settings
+        self.screen_width = 1920
+        self.screen_height = 900
+        self.bg_colour = (0, 0, 0)
+
+        # Ship Settings
+        self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (250, 250, 250)
+        self.bullets_allowed = 6
